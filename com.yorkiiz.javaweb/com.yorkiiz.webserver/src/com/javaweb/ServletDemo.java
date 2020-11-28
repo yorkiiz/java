e7c00b26a9a13e30c835f565f872b26e38b029ff
yorkiiz <297555475@qq.com>
2020-11-28 15:58:07 +0800

    servlet入门

---
+++ com.yorkiiz.javaweb/com.yorkiiz.webserver/src/com/javaweb/ServletDemo.java
@@ -0,0 +1,41 @@
+package com.javaweb;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @auther:
+ * @date:
+ * @describtion:
+ **/
+
+public class ServletDemo implements Servlet {
+
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+        System.out.println("Hellow Servlet");
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
